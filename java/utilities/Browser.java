1a0be2290e4f2696b82b19fc5ecbcdbe6f175bf0
Lukasz <luck.lk3112@gmail.com>
2018-04-14 23:36:57 +0200

    adding the Browser utility

---
+++ java/utilities/Browser.java
@@ -0,0 +1,36 @@
+package utilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Browser {
+
+    static String hubUrl = "http://localhost:4444/wd/hub";
+
+    public static RemoteWebDriver getDriver(String browser) throws MalformedURLException {
+        return new RemoteWebDriver(new URL(hubUrl), getBrowserCapabilities(browser));
+    }
+
+    private static DesiredCapabilities getBrowserCapabilities(String browserType) {
+
+        if (browserType.equals("firefox")) {
+            System.out.println("Opening firefox driver");
+            return DesiredCapabilities.firefox();
+        }
+        else if (browserType.equals("chrome")) {
+            System.out.println("Opening chrome driver");
+            return DesiredCapabilities.chrome();
+        }
+        else if(browserType.equals("IE")){
+            System.out.println("Opening IE driver");
+            return DesiredCapabilities.internetExplorer();
+        }
+        else{
+            System.out.println("browser : " + browserType + " is invalid, Launching Firefox as browser of choice..");
+            return DesiredCapabilities.firefox();
+        }
+    }
+}
