22a06383a5c69f03cbd88cdd18571bccc811ed50
Lukasz <luck.lk3112@gmail.com>
2018-04-15 14:00:42 +0200

    folder rename fix

--- java/pageObjects/RegisteryPage.java
+++ java/pageObjects/RegisteryPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
-import static utilities.Utilities.addRandomData;
+import static utility.Utilities.addRandomData;
 
 public class RegisteryPage extends PageObject {
 
